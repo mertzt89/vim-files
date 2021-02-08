e6f5d1ae9606c2fc37f5f81be3163cd117f4f778
Timothy Mertz <mertzt89@gmail.com>
2021-02-08 16:28:00 -0600

    Add python support

---
+++ lua/modules/lang/python/init.lua
@@ -0,0 +1,20 @@
+--- Python module
+
+local file = require("lib.file")
+
+local module = {}
+
+--- Returns plugins required for this module
+function module.register_plugins()
+end
+
+--- Configures vim and plugins for this module
+function module.init()
+  local lsp = require("modules.lsp")
+  local build = require("modules.build")
+  local lspconfig = require("lspconfig")
+
+  lsp.register_server(lspconfig.pyls, {})
+end
+
+return module
