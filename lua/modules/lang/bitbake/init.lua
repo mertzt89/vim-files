105fc10c0e8a5716ac3cc7e53b6f0266b678c8be
Timothy Mertz <mertzt89@gmail.com>
2021-02-08 16:28:00 -0600

    Add bitbake support

---
+++ lua/modules/lang/bitbake/init.lua
@@ -0,0 +1,16 @@
+--- Bitbake module
+
+local plugman = require("lib.plugman")
+
+local module = {}
+
+--- Returns plugins required for this module
+function module.register_plugins()
+    plugman.use({'kergoth/vim-bitbake'})
+end
+
+--- Configures vim and plugins for this module
+function module.init()
+end
+
+return module
