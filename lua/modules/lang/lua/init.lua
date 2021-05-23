77c113c331e950a5fe6128fe12446c4e60048e87
Timothy Mertz <mertzt89@gmail.com>
2021-05-23 12:11:42 -0500

    lua: Add LSP support
    Instructions:
      - Add script called 'lls' to path
            #!/bin/bash
            LLS_DIR=$HOME/src/lua-language-server
            $LLS_DIR/bin/Linux/lua-language-server -E $LLS_DIR/main.lua

--- lua/modules/lang/lua/init.lua
+++ lua/modules/lang/lua/init.lua
@@ -1,13 +1,6 @@
 --- Lua module
-local autocmd = require("lib.autocmd")
 
 local module = {}
-
-local function on_filetype_lua()
-  vim.api.nvim_buf_set_option(0, "shiftwidth", 2)
-  vim.api.nvim_buf_set_option(0, "tabstop", 2)
-  vim.api.nvim_buf_set_option(0, "softtabstop", 4)
-end
 
 --- Returns plugins required for this module
 function module.register_plugins() end
@@ -17,23 +10,33 @@ function module.init()
   local lsp = require("modules.lsp")
   local lspconfig = require("lspconfig")
 
-  -- TODO: Make this configurable per-project
-  -- lsp.register_server(
-  --  lspconfig.sumneko_lua,
-  --  {
-  --    settings = {
-  --      Lua = {
-  --        diagnostics = {
-  --          globals = {
-  --            "vim",
-  --          },
-  --        },
-  --      },
-  --    },
-  --  }
-  -- )
+  lsp.register_server(lspconfig.sumneko_lua, {
+    cmd = {'lls'},
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = vim.split(package.path, ';')
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'}
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true
+          }
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {enable = false}
+      }
+    }
+  })
 
-  -- autocmd.bind_filetype("lua", on_filetype_lua)
 end
 
 return module
