6e438cce682cb88fcacf15e990e1f604947d3a8c
Timothy Mertz <mertzt89@gmail.com>
2021-05-23 12:10:51 -0500

    .nvim.lua: Set indentation rules

--- .nvim.lua
+++ .nvim.lua
@@ -1,2 +1,10 @@
 autocmd = require'lib.autocmd'
 autocmd.bind("BufWritePost *.lua", require'packer'.compile())
+
+local function on_filetype_lua()
+  vim.api.nvim_buf_set_option(0, "shiftwidth", 2)
+  vim.api.nvim_buf_set_option(0, "tabstop", 2)
+  vim.api.nvim_buf_set_option(0, "softtabstop", 4)
+end
+
+autocmd.bind_filetype("lua", on_filetype_lua)
