9d9656d861d22752d63120ed64e986032c42de79
Timothy Mertz <mertzt89@gmail.com>
2021-05-27 13:30:58 -0500

    telescope: Break out configuration

--- lua/modules/core/init.lua
+++ lua/modules/core/init.lua
@@ -16,120 +16,6 @@ local function set_default_buf_opt(name, value)
   autocmd.bind_vim_enter(function() vim.bo[name] = value end)
 end
 
-function _G.grep_operator(t, ...)
-  local regsave = vim.fn.getreg("@")
-  local selsave = vim.o.selection
-
-  vim.o.selection = 'inclusive'
-
-  if t == 'v' or t == 'V' then
-    vim.api.nvim_command('silent execute "normal! gvy"')
-  elseif t == 'line' then
-    vim.api.nvim_command('silent execute "normal! \'[V\']y"')
-  else
-    vim.api.nvim_command('silent execute "normal! `[v`]y"')
-  end
-
-  vim.o.selection = selsave
-  local query = vim.fn.getreg("@")
-
-  vim.fn.setreg("@", regsave)
-  print(query)
-
-  require'telescope.builtin'.grep_string({
-    find_command = "rg,--no-ignore,--hidden,--no-heading,--vimgrep",
-    search = query
-  })
-end
-
-local function register_telescope()
-  -- Telescope
-  plugman.use({
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
-    config = function()
-      local actions = require 'telescope.actions'
-      local keybind = require 'lib.keybind'
-
-      require'telescope'.setup {
-        defaults = {
-          vimgrep_arguments = {
-            'rg', '--color=never', '--no-heading', '--with-filename',
-            '--line-number', '--column', '--smart-case', '--no-ignore-vcs'
-          },
-          file_sorter = require'telescope.sorters'.get_fuzzy_file,
-          mappings = {
-            i = {
-              ["<C-n>"] = actions.move_selection_next,
-              ["<C-j>"] = actions.move_selection_next,
-              ["<C-p>"] = actions.move_selection_previous,
-              ["<C-k>"] = actions.move_selection_previous,
-
-              ["<C-c>"] = actions.close,
-
-              ["<Down>"] = actions.move_selection_next,
-              ["<Up>"] = actions.move_selection_previous,
-
-              ["<CR>"] = actions.select_default + actions.center,
-              ["<C-x>"] = actions.select_horizontal,
-              ["<C-v>"] = actions.select_vertical,
-              ["<C-t>"] = actions.select_tab,
-
-              ["<C-u>"] = actions.preview_scrolling_up,
-              ["<C-d>"] = actions.preview_scrolling_down,
-
-              ["<Tab>"] = actions.toggle_selection +
-                actions.move_selection_worse,
-              ["<S-Tab>"] = actions.toggle_selection +
-                actions.move_selection_better,
-              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-              ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-              ["<C-l>"] = actions.complete_tag
-            },
-
-            n = {
-              ["<esc>"] = actions.close,
-              ["q"] = actions.close,
-              ["<CR>"] = actions.select_default + actions.center,
-              ["<C-x>"] = actions.select_horizontal,
-              ["<C-v>"] = actions.select_vertical,
-              ["<C-t>"] = actions.select_tab,
-
-              ["<Tab>"] = actions.toggle_selection +
-                actions.move_selection_worse,
-              ["<S-Tab>"] = actions.toggle_selection +
-                actions.move_selection_better,
-              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-              ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-              -- TODO: This would be weird if we switch the ordering.
-              ["j"] = actions.move_selection_next,
-              ["<C-j>"] = actions.move_selection_next,
-              ["k"] = actions.move_selection_previous,
-              ["<C-k>"] = actions.move_selection_previous,
-              ["H"] = actions.move_to_top,
-              ["M"] = actions.move_to_middle,
-              ["L"] = actions.move_to_bottom,
-
-              ["<Down>"] = actions.move_selection_next,
-              ["<Up>"] = actions.move_selection_previous,
-
-              ["<C-u>"] = actions.preview_scrolling_up,
-              ["<C-d>"] = actions.preview_scrolling_down
-            }
-          }
-        }
-      } -- end telescope setup
-      keybind.bind_command(keybind.mode.NORMAL, "<C-p>",
-                           ":Telescope find_files find_command=rg,--files,-L,--no-ignore-vcs,--hidden<CR>",
-                           {noremap = true, silent = true})
-      keybind.bind_command(keybind.mode.NORMAL, "<F3>",
-                           ":Telescope buffers show_all_buffers=true<CR>",
-                           {noremap = true, silent = true})
-    end
-  })
-end
-
 function module.register_plugins()
   plugman.use({
     'lambdalisue/fern.vim',
@@ -195,7 +81,8 @@ function module.register_plugins()
   plugman.use({'justinmk/vim-sneak'})
 
   -- Fuzzy grepping, file finding, etc..
-  register_telescope()
+  local t = require'modules.core.telescope'
+  t.register_telescope()
 end
 
 function module.init()
@@ -204,13 +91,6 @@ function module.init()
   vim.cmd("filetype plugin on")
 
   vim.o.viminfo = "'100,<500,s10,h,!"
-
-  -- keybind.bind_command(keybind.mode.NORMAL, "gs", ":set opfunc=v:lua.require'modules.core'.grep_operator<CR>g@")
-  keybind.bind_command(keybind.mode.NORMAL, "gs",
-                       ":set opfunc=v:lua.grep_operator<CR>g@", {silent = true})
-  keybind.bind_command(keybind.mode.VISUAL, "gs",
-                       ":<c-u>call v:lua.grep_operator(visualmode())<CR>",
-                       {silent = true})
 
   -- Default indentation options
   set_default_buf_opt("tabstop", 4)
---
+++ lua/modules/core/telescope.lua
@@ -0,0 +1,130 @@
+local plugman = require 'lib.plugman'
+
+local M = {}
+
+local function grep_operator(t, ...)
+  local regsave = vim.fn.getreg("@")
+  local selsave = vim.o.selection
+
+  vim.o.selection = 'inclusive'
+
+  if t == 'v' or t == 'V' then
+    vim.api.nvim_command('silent execute "normal! gvy"')
+  elseif t == 'line' then
+    vim.api.nvim_command('silent execute "normal! \'[V\']y"')
+  else
+    vim.api.nvim_command('silent execute "normal! `[v`]y"')
+  end
+
+  vim.o.selection = selsave
+  local query = vim.fn.getreg("@")
+
+  vim.fn.setreg("@", regsave)
+  print(query)
+
+  require'telescope.builtin'.grep_string({
+    find_command = "rg,--no-ignore,--hidden,--no-heading,--vimgrep",
+    search = query
+  })
+end
+
+function M.register_telescope()
+  -- Telescope
+  plugman.use({
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+    config = function()
+      local actions = require 'telescope.actions'
+      local keybind = require 'lib.keybind'
+
+      require'telescope'.setup {
+        defaults = {
+          vimgrep_arguments = {
+            'rg', '--color=never', '--no-heading', '--with-filename',
+            '--line-number', '--column', '--smart-case', '--no-ignore-vcs'
+          },
+          file_sorter = require'telescope.sorters'.get_fuzzy_file,
+          mappings = {
+            i = {
+              ["<C-n>"] = actions.move_selection_next,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-p>"] = actions.move_selection_previous,
+              ["<C-k>"] = actions.move_selection_previous,
+
+              ["<C-c>"] = actions.close,
+
+              ["<Down>"] = actions.move_selection_next,
+              ["<Up>"] = actions.move_selection_previous,
+
+              ["<CR>"] = actions.select_default + actions.center,
+              ["<C-x>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+              ["<C-t>"] = actions.select_tab,
+
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down,
+
+              ["<Tab>"] = actions.toggle_selection +
+                actions.move_selection_worse,
+              ["<S-Tab>"] = actions.toggle_selection +
+                actions.move_selection_better,
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+              ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+              ["<C-l>"] = actions.complete_tag
+            },
+
+            n = {
+              ["<esc>"] = actions.close,
+              ["q"] = actions.close,
+              ["<CR>"] = actions.select_default + actions.center,
+              ["<C-x>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+              ["<C-t>"] = actions.select_tab,
+
+              ["<Tab>"] = actions.toggle_selection +
+                actions.move_selection_worse,
+              ["<S-Tab>"] = actions.toggle_selection +
+                actions.move_selection_better,
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+              ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+              -- TODO: This would be weird if we switch the ordering.
+              ["j"] = actions.move_selection_next,
+              ["<C-j>"] = actions.move_selection_next,
+              ["k"] = actions.move_selection_previous,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["H"] = actions.move_to_top,
+              ["M"] = actions.move_to_middle,
+              ["L"] = actions.move_to_bottom,
+
+              ["<Down>"] = actions.move_selection_next,
+              ["<Up>"] = actions.move_selection_previous,
+
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down
+            }
+          }
+        }
+      } -- end telescope setup
+      keybind.bind_command(keybind.mode.NORMAL, "<C-p>",
+                           ":Telescope find_files find_command=rg,--files,-L,--no-ignore-vcs,--hidden<CR>",
+                           {noremap = true, silent = true})
+      keybind.bind_command(keybind.mode.NORMAL, "<F3>",
+                           ":Telescope buffers show_all_buffers=true<CR>",
+                           {noremap = true, silent = true})
+
+
+      -- Grep operator
+      keybind.bind_command(keybind.mode.NORMAL, "gs",
+                          ":set opfunc=v:lua.telescope_grep_op<CR>g@", {silent = true})
+      keybind.bind_command(keybind.mode.VISUAL, "gs",
+                          ":<c-u>call v:lua.telescope_grep_op(visualmode())<CR>",
+                          {silent = true})
+
+    end
+  })
+
+  _G.telescope_grep_op = function(t, ...) grep_operator(t,...) end
+end
+
+return M
