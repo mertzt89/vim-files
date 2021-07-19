dcfba0b7b04b9d3b371e493c7d475fb5fc0a5f70
Timothy Mertz <mertzt89@gmail.com>
2021-07-19 17:34:27 -0500

    python: Replace pyls with pylsp

--- lua/modules/lang/python/init.lua
+++ lua/modules/lang/python/init.lua
@@ -1,6 +1,4 @@
 --- Python module
-local file = require("lib.file")
-
 local module = {}
 
 --- Returns plugins required for this module
@@ -9,10 +7,9 @@ function module.register_plugins() end
 --- Configures vim and plugins for this module
 function module.init()
   local lsp = require("modules.lsp")
-  local build = require("modules.build")
   local lspconfig = require("lspconfig")
 
-  lsp.register_server(lspconfig.pyls, {})
+  lsp.register_server(lspconfig.pylsp, {})
 end
 
 return module
