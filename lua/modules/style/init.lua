73abbdd64d40f18bf83b721baadfedeaf2736cab
Timothy Mertz <mertzt89@gmail.com>
2022-06-01 09:31:02 -0500

    treesitter: Install 'all' parsers

--- lua/modules/style/init.lua
+++ lua/modules/style/init.lua
@@ -50,7 +50,7 @@ function module.register_plugins()
     'nvim-treesitter/nvim-treesitter',
     config = function()
       require'nvim-treesitter.configs'.setup {
-        ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
         ignore_install = {"comment"}, -- List of parsers to ignore installing
         highlight = {
           enable = true, -- false will disable the whole extension
