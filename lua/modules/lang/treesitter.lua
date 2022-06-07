7b2f8e8302c50fe7135081cc9a87f3add7521382
Timothy Mertz <mertzt89@gmail.com>
2022-06-07 11:01:08 -0500

    Add treesitter-textobjects

--- lua/modules/lang/treesitter.lua
+++ lua/modules/lang/treesitter.lua
@@ -17,6 +17,39 @@ function module.register_plugins(use)
     end,
     run = ':TSUpdate'
   })
+
+  use {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        textobjects = {
+          select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner"
+            }
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {["]]"] = "@function.outer"},
+            goto_next_end = {["]["] = "@function.outer"},
+            goto_previous_start = {["[["] = "@function.outer"},
+            goto_previous_end = {["[]"] = "@function.outer"}
+          }
+        }
+      }
+    end
+  }
+
 end
 
 function module.init() end
