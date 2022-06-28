76cb2b7fe623a31b1ea5237bc48af50a20ac28b0
Timothy Mertz <mertzt89@gmail.com>
2022-06-28 09:20:49 -0500

    Add indent-blankline

--- lua/modules/style/init.lua
+++ lua/modules/style/init.lua
@@ -14,6 +14,16 @@ function module.register_plugins(use)
     end
   })
 
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup {
+        show_current_context = true,
+        show_current_context_start = true
+      }
+    end
+  }
+
   -- Hightlight word under cursor
   use({
     'RRethy/vim-illuminate',
