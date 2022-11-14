cdd678dc364c9610048ede5ecb3f470e1711da1e
Timothy Mertz <mertzt89@gmail.com>
2022-11-14 15:44:08 -0600

    clangd: Add query-driver argument for OE SDKs

--- lua/modules/lang/c/init.lua
+++ lua/modules/lang/c/init.lua
@@ -6,13 +6,23 @@ local module = {}
 --- Returns plugins required for this module
 function module.register_plugins(use) end
 
+local function get_clangd_command()
+    local cmd = { "clangd" }
+    if vim.env.OECORE_NATIVE_SYSROOT ~= nil then
+        table.insert(cmd, "--query-driver=" .. vim.env.OECORE_NATIVE_SYSROOT .. "/**/arm-*")
+    end
+    return cmd
+end
+
 --- Configures vim and plugins for this module
 function module.init()
     local lsp = require("modules.lsp")
     local build = require("modules.build")
     local lspconfig = require("lspconfig")
 
-    lsp.register_server(lspconfig.clangd)
+    lsp.register_server(lspconfig.clangd, {
+        cmd = get_clangd_command()
+    })
 
     -- Ignore common build output directories
     file.add_to_wildignore(".build")
