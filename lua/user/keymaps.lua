a9968570d811878cf2ea09d627bdf71c09cc730c
Timothy Mertz <mertzt89@gmail.com>
2023-05-31 16:43:44 -0500

    keymap: Add window navigation

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -14,3 +14,12 @@ vim.keymap.set({ "n", "x" }, "cv", '"+p')
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
 vim.keymap.set("n", "<leader>bq", "<cmd>bdelete<cr>")
 vim.keymap.set("n", "<leader>bl", "<cmd>buffer #<cr>")
+
+-- Window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Window Left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Window Down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Window Up" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Window Right" })
+
+-- Screen refresh
+vim.keymap.set("n", "<leader><C-l>", "<Cmd>nohlsearch|diffupdate|normal! <C-L><CR>", { desc = "Refresh Screen" })
