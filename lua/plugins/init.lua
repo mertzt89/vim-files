0260a5e0306bc566a23c99047e36a311dc21cf6e
Timothy Mertz <mertzt89@gmail.com>
2023-05-31 16:43:44 -0500

    plugin: Add vim-visual-multi

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,7 @@ local Plugins = {
   { "tpope/vim-repeat" },
   { "kyazdani42/nvim-web-devicons", lazy = true },
   { "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
+  { "mg979/vim-visual-multi", lazy = false },
 
   -- Themes
   { "folke/tokyonight.nvim" },
