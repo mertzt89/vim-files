ab73df1564eaf9f551e3fac4e89290bd05cd13a3
Timothy Mertz <mertzt89@gmail.com>
2023-05-31 16:43:44 -0500

    toggleterm: keymap: Change toggle

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,11 +2,11 @@ local Plugin = { "akinsho/toggleterm.nvim" }
 
 Plugin.name = "toggleterm"
 
-Plugin.keys = { "<C-g>" }
+Plugin.keys = { "<C-\\>" }
 
 -- See :help toggleterm-roadmap
 Plugin.opts = {
-  open_mapping = "<C-g>",
+  open_mapping = "<C-\\>",
   direction = "horizontal",
   shade_terminals = true,
 }
