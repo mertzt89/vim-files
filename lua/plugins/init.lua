1c35aad0a1847692fcc937fafb566dc40d7c27a5
Timothy Mertz <mertzt89@gmail.com>
2023-06-01 10:03:06 -0500

    cleanup: Remove unused themes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,12 +5,6 @@ local Plugins = {
   { "kyazdani42/nvim-web-devicons", lazy = true },
   { "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
   { "mg979/vim-visual-multi", lazy = false },
-
-  -- Themes
-  { "joshdick/onedark.vim" },
-  { "tanvirtin/monokai.nvim" },
-  { "lunarvim/darkplus.nvim" },
-  { "nyoom-engineering/oxocarbon.nvim" },
 }
 
 return Plugins
