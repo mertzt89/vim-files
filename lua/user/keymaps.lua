aac8bc34e15664c29128c7ed0f9dde76b8609cae
Timothy Mertz <mertzt89@gmail.com>
2023-10-19 16:26:41 -0500

    format with stylua

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -2,10 +2,10 @@
 vim.g.mapleader = " "
 
 -- Window Navigation
-vim.keymap.set({"n", "x", "o"}, "<C-h>", "<C-w>h")
-vim.keymap.set({"n", "x", "o"}, "<C-j>", "<C-w>j")
-vim.keymap.set({"n", "x", "o"}, "<C-k>", "<C-w>k")
-vim.keymap.set({"n", "x", "o"}, "<C-l>", "<C-w>l")
+vim.keymap.set({ "n", "x", "o" }, "<C-h>", "<C-w>h")
+vim.keymap.set({ "n", "x", "o" }, "<C-j>", "<C-w>j")
+vim.keymap.set({ "n", "x", "o" }, "<C-k>", "<C-w>k")
+vim.keymap.set({ "n", "x", "o" }, "<C-l>", "<C-w>l")
 
 -- Commands
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
@@ -13,5 +13,5 @@ vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>")
 vim.keymap.set("n", "<leader>bl", "<cmd>buffer #<cr>")
 
 -- Toggles
-vim.keymap.set("n", "<leader>uf", ":set foldenable!<CR>", {silent=true}) -- Toggle Folding
-vim.keymap.set("n", "<leader>un", ":set relativenumber!<CR>", {silent=true}) -- Toggle Relative Number
+vim.keymap.set("n", "<leader>uf", ":set foldenable!<CR>", { silent = true }) -- Toggle Folding
+vim.keymap.set("n", "<leader>un", ":set relativenumber!<CR>", { silent = true }) -- Toggle Relative Number
