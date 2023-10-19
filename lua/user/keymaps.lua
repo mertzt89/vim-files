2a613538d39314db7cde5fd9aaf6b9d39eaf5e07
Timothy Mertz <mertzt89@gmail.com>
2023-10-19 15:10:38 -0500

    keymap: Add window navigation

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,6 +1,11 @@
 -- Space as leader key
 vim.g.mapleader = " "
 
+-- Window Navigation
+vim.keymap.set({"n", "x", "o"}, "<C-h>", "<C-w>h")
+vim.keymap.set({"n", "x", "o"}, "<C-j>", "<C-w>j")
+vim.keymap.set({"n", "x", "o"}, "<C-k>", "<C-w>k")
+vim.keymap.set({"n", "x", "o"}, "<C-l>", "<C-w>l")
 
 -- Commands
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
