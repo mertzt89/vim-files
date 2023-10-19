f588020c5b60bc58d30de167cc644ae15c7d1958
Timothy Mertz <mertzt89@gmail.com>
2023-10-19 18:41:26 -0500

    toggleterm: Update mapping

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,11 +2,11 @@ local Plugin = { "akinsho/toggleterm.nvim" }
 
 Plugin.name = "toggleterm"
 
-Plugin.keys = { "<C-g>" }
+Plugin.keys = { "<C-_>" }
 
 -- See :help toggleterm-roadmap
 Plugin.opts = {
-	open_mapping = "<C-g>",
+	open_mapping = "<C-_>",
 	direction = "horizontal",
 	shade_terminals = true,
 }
