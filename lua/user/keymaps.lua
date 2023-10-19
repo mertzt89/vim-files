6c13f31f482b46e1b3fee615b878a379afdf83e2
Timothy Mertz <mertzt89@gmail.com>
2023-10-19 15:10:48 -0500

    keymap: Add buffer delete

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -9,5 +9,5 @@ vim.keymap.set({"n", "x", "o"}, "<C-l>", "<C-w>l")
 
 -- Commands
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
-vim.keymap.set("n", "<leader>bq", "<cmd>bdelete<cr>")
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>")
 vim.keymap.set("n", "<leader>bl", "<cmd>buffer #<cr>")
