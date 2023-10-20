1f0f5f2df2c1377b91ffb912b56d889c45b991d2
Timothy Mertz <mertzt89@gmail.com>
2023-10-19 19:04:33 -0500

    plugin: Add overseer

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,5 @@
 local Plugin = { "nvim-lualine/lualine.nvim" }
 
-Plugin.name = "lualine"
-
 Plugin.event = "VeryLazy"
 
 -- See :help lualine.txt
@@ -15,10 +13,21 @@ Plugin.opts = {
 			statusline = { "NvimTree" },
 		},
 	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { "filename" },
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
 }
 
 function Plugin.init()
 	vim.opt.showmode = false
 end
+
+-- Force this layer of config to load first
+Plugin.priiority = 100
 
 return Plugin
