81fae463e85029b8213fb27a92613cf9d0ffad7c
Timothy Mertz <mertzt89@gmail.com>
2023-10-21 00:10:06 -0500

    treesitter: De-dupe ensure list

--- lua/util/spec.lua
+++ lua/util/spec.lua
@@ -4,10 +4,12 @@ function M.ts_ensure_installed(ensure)
 	return {
 		"nvim-treesitter/nvim-treesitter",
 		opts = function(_, opts)
-			if type(opts.ensure_installed) == "table" then
-				vim.list_extend(opts.ensure_installed, ensure)
-			else
-				opts.ensure_installed = ensure
+			opts.ensure_installed = opts.ensure_installed or {}
+
+			for _, e in pairs(ensure) do
+				if not vim.list_contains(opts.ensure_installed, e) then
+					table.insert(opts.ensure_installed, e)
+				end
 			end
 		end,
 	}
