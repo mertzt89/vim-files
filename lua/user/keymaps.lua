944d4236649172a332c05b7e74a42cb642b36ea4
Timothy Mertz <mertzt89@gmail.com>
2023-10-23 15:10:31 -0500

    Add maps to move to next and previous buffers

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -7,10 +7,14 @@ vim.keymap.set({ "n", "x", "o" }, "<C-j>", "<C-w>j")
 vim.keymap.set({ "n", "x", "o" }, "<C-k>", "<C-w>k")
 vim.keymap.set({ "n", "x", "o" }, "<C-l>", "<C-w>l")
 
+-- Buffer switching
+vim.keymap.set("n", "<S-l>", "<cmd>bn<cr>")
+vim.keymap.set("n", "<S-h>", "<cmd>bp<cr>")
+vim.keymap.set("n", "<leader>bl", "<cmd>buffer #<cr>")
+
 -- Commands
 vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>")
-vim.keymap.set("n", "<leader>bl", "<cmd>buffer #<cr>")
 
 -- Toggles
 vim.keymap.set("n", "<leader>uf", ":set foldenable!<CR>", { silent = true }) -- Toggle Folding
