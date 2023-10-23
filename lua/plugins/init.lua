b26867efb17f1acb0b3d9a093863db7dab9aa8a4
Timothy Mertz <mertzt89@gmail.com>
2023-10-23 15:10:06 -0500

    fix: Early initialize base lualine config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,30 @@
 return {
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				theme = "tokyonight",
+				icons_enabled = true,
+				component_separators = "|",
+				section_separators = "",
+				disabled_filetypes = {
+					statusline = { "NvimTree" },
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		},
+		init = function()
+			vim.opt.showmode = false
+		end,
+	},
 	{ import = ... .. ".code" },
 	{ import = ... .. ".editor" },
 	{ import = ... .. ".themes" },
