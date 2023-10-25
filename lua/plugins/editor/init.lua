5cf23abbb526fc986f9538f1656afccb5cd1f6b7
Timothy Mertz <mertzt89@gmail.com>
2023-10-25 16:27:41 -0500

    plugin: Add grep-op.nvim

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -5,4 +5,9 @@ return {
 	{ "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
 	{ "kyazdani42/nvim-web-devicons", lazy = true },
 	{ "mg979/vim-visual-multi", lazy = false },
+	{
+		"mertzt89/grep-op.nvim",
+		config = true,
+		lazy = false,
+	},
 }
