68a41b52bbd9916911be8cedd34d0b69fb070e47
Timothy Mertz <mertzt89@gmail.com>
2023-11-06 09:35:02 -0600

    lang: python: Add initial support

---
+++ lua/plugins/code/lang/python.lua
@@ -0,0 +1,31 @@
+return {
+	-- Treesitter
+	require("util.spec").ts_ensure_installed({ "ninja", "python", "rst", "toml" }),
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				pyright = {},
+				ruff_lsp = {
+					keys = {
+						{
+							"<leader>co",
+							function()
+								vim.lsp.buf.code_action({
+									apply = true,
+									context = {
+										only = { "source.organizeImports" },
+										diagnostics = {},
+									},
+								})
+							end,
+							desc = "Organize Imports",
+						},
+					},
+				},
+			},
+		},
+	},
+}
