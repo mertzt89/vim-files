bc7543b8c32a7a85ab22f6e73e1917bf530cb8d3
Timothy Mertz <mertzt89@gmail.com>
2023-11-17 14:15:13 -0600

    nvim-tree: Add find file binding

--- lua/plugins/editor/nvim-tree.lua
+++ lua/plugins/editor/nvim-tree.lua
@@ -2,13 +2,14 @@ local Plugin = { "kyazdani42/nvim-tree.lua" }
 
 Plugin.name = "nvim-tree"
 
-Plugin.cmd = { "NvimTreeToggle" }
+Plugin.cmd = { "NvimTreeToggle", "NvimTreeFindFile" }
 
 -- Use defualt config
 Plugin.config = true
 
 function Plugin.init()
-	vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
+	vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFile<cr>")
+	vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeToggle<cr>")
 end
 
 return Plugin
