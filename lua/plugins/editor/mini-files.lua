fb62a629962f58c5ff59149620b8186f2c876100
Timothy Mertz <mertzt89@gmail.com>
2024-01-11 09:37:25 -0600

    mini-files: Add binding to toggle hidden

--- lua/plugins/editor/mini-files.lua
+++ lua/plugins/editor/mini-files.lua
@@ -1,3 +1,19 @@
+local show_dotfiles = true
+
+local filter_show = function(_)
+	return true
+end
+
+local filter_hide = function(fs_entry)
+	return not vim.startswith(fs_entry.name, ".")
+end
+
+local toggle_dotfiles = function()
+	show_dotfiles = not show_dotfiles
+	local new_filter = show_dotfiles and filter_show or filter_hide
+	MiniFiles.refresh({ content = { filter = new_filter } })
+end
+
 return {
 	"echasnovski/mini.files",
 	keys = {
@@ -10,5 +26,18 @@ return {
 		},
 	},
 	opts = {},
+	config = function(_, opts)
+		require("mini.files").setup(opts)
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniFilesBufferCreate",
+			callback = function(args)
+				vim.notify("mini")
+				local buf_id = args.data.buf_id
+				-- Tweak left-hand side of mapping to your liking
+				vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+			end,
+		})
+	end,
 	version = false,
 }
