1d23a23c23d2698110dc7a27c1c0dd7978a190f7
Timothy Mertz <mertzt89@gmail.com>
2024-01-14 13:37:28 -0600

    plugin: css: Add nvim-highlight-colors

--- lua/plugins/code/lang/css.lua
+++ lua/plugins/code/lang/css.lua
@@ -6,4 +6,13 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   end,
 })
 
-return {}
+return {
+  {
+    "brenoprata10/nvim-highlight-colors",
+    opts = {
+      render = "background", -- or 'foreground' or 'first_column'
+      enable_named_colors = true,
+      enable_tailwind = true,
+    },
+  },
+}
