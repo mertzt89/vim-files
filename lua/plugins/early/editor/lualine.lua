1291758ece3d209a7a4d490f336227f944ec1c27
Timothy Mertz <mertzt89@gmail.com>
2024-01-14 13:12:37 -0600

    lualine: Move spec to own file

---
+++ lua/plugins/early/editor/lualine.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        theme = "tokyonight",
+        icons_enabled = true,
+        component_separators = "|",
+        section_separators = "",
+        disabled_filetypes = {
+          statusline = { "NvimTree" },
+        },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    },
+    init = function()
+      vim.opt.showmode = false
+    end,
+  },
+}
