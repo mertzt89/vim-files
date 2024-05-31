4374565572addb117593f79588f30e956e444180
Timothy Mertz <mertzt89@gmail.com>
2024-05-31 01:39:06 -0500

    plugin: Add CopilotChat.nvim

--- lua/plugins/code/copilot.lua
+++ lua/plugins/code/copilot.lua
@@ -91,4 +91,17 @@ return require("util").opt_in("copilot", {
       })
     end,
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 })
