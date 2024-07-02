b916aa97ce42698ed06e7fa3a927e8caa09d92dc
Timothy Mertz <mertzt89@gmail.com>
2024-07-02 12:59:44 -0500

    lualine: Use auto for theme

--- lua/plugins/early/editor/lualine.lua
+++ lua/plugins/early/editor/lualine.lua
@@ -12,7 +12,7 @@ return {
     event = "VeryLazy",
     opts = {
       options = {
-        theme = "tokyonight",
+        theme = "auto",
         icons_enabled = true,
         component_separators = "|",
         section_separators = "",
