08d514bb1d408fbd906d426e455d7c76ce71c6d3
Timothy Mertz <mertzt89@gmail.com>
2024-07-02 13:04:51 -0500

    Remove custom highlight for "mini indent"
    Indent blank line is used in favor of mini indent, so the custom
    highlight for mini indent is removed

--- lua/plugins/themes/tokyonight.lua
+++ lua/plugins/themes/tokyonight.lua
@@ -1,14 +1,6 @@
 return {
   "folke/tokyonight.nvim",
   opts = {
-    on_highlights = function(hl, c)
-      -- Instead of showing the current indent scope with blue,
-      -- use a slightly brightened version IndentBlanklineChar
-      hl.MiniIndentscopeSymbol = {
-        fg = require("tokyonight.util").lighten(hl.IndentBlanklineChar.fg, 0.8),
-      }
-    end,
-
     -- Default to "night" i.e. `:colorscheme tokyonight-night`
     style = "night",
   },
