f48478a25e6065f31c900d9b20a843b69fbb011b
Timothy Mertz <mertzt89@gmail.com>
2024-07-23 17:03:00 -0500

    fzf: Add mapping to access vim registers

--- lua/plugins/editor/fzf.lua
+++ lua/plugins/editor/fzf.lua
@@ -1,5 +1,38 @@
 local is_win = require("util").is_win()
 
+local cword = ""
+
+-- FZF command wrapper that merges common configurations
+local fzf_cmd = function(fzf_command, opts)
+  opts = opts or {}
+
+  opts = vim.tbl_deep_extend("keep", opts, {
+    winopts = {
+      on_create = function()
+        local b = vim.api.nvim_get_current_buf()
+
+        -- Access vim registers from FZF prompt
+        vim.keymap.set("t", "<C-r>", function()
+          vim.schedule(function()
+            local char = vim.fn.getchar()
+            local key = vim.fn.nr2char(char)
+            if key == "" then -- <C-w>
+              vim.fn.feedkeys(cword)
+            else
+              vim.fn.feedkeys(vim.fn.getreg(key))
+            end
+          end)
+        end, { buffer = b, expr = true })
+      end,
+    },
+  })
+
+  return function()
+    cword = vim.fn.expand("<cword>")
+    require("fzf-lua")[fzf_command](opts)
+  end
+end
+
 -- Custom find files command handling
 local find_files_command = function(extra_args, opts)
   extra_args = extra_args or {}
@@ -8,10 +41,7 @@ local find_files_command = function(extra_args, opts)
   local cmd = vim.list_extend({ "rg", "--color", "never", "--files" }, extra_args)
   opts = vim.tbl_deep_extend("keep", opts, { cmd = table.concat(cmd, " ") })
 
-  return function()
-    local b = require("fzf-lua")
-    b.files(opts)
-  end
+  return fzf_cmd("files", opts)
 end
 
 -- Different "find files" commands to be bound
@@ -32,21 +62,21 @@ return {
       local fzf = require("fzf-lua")
       return {
         -- Buffer
-        { "<leader><space>", fzf.buffers, { desc = "Buffers" } },
-        { "<leader>sb", fzf.blines, desc = "Find in Buffer" },
-        { "<leader>fs", fzf.blines, { desc = "Find in Buffer" } },
+        { "<leader><space>", fzf_cmd("buffers"), { desc = "Buffers" } },
+        { "<leader>sb", fzf_cmd("blines"), desc = "Find in Buffer" },
+        { "<leader>fs", fzf_cmd("blines"), { desc = "Find in Buffer" } },
 
         -- Commands
-        { "<leader>sa", fzf.autocmds, desc = "Auto Commands" },
-        { "<leader>sC", fzf.commands, desc = "Commands" },
-        { "<leader>sc", fzf.command_history, desc = "Command History" },
+        { "<leader>sa", fzf_cmd("autocmds"), desc = "Auto Commands" },
+        { "<leader>sC", fzf_cmd("commands"), desc = "Commands" },
+        { "<leader>sc", fzf_cmd("command_history"), desc = "Command History" },
 
         -- Diagnostics
-        { "<leader>sD", fzf.diagnostics_workspace, desc = "Workspace diagnostics" },
-        { "<leader>sd", fzf.diagnostics_document, { desc = "Diagnostics" } },
+        { "<leader>sD", fzf_cmd("diagnostics_workspace"), desc = "Workspace diagnostics" },
+        { "<leader>sd", fzf_cmd("diagnostics_document"), { desc = "Diagnostics" } },
 
         -- Find Files
-        { "<leader>?", fzf.oldfiles, { desc = "Recent Files" } },
+        { "<leader>?", fzf_cmd("oldfiles"), { desc = "Recent Files" } },
         {
           "<leader>ff",
           find_files.no_ignore_vcs,
@@ -58,38 +88,36 @@ return {
         -- Grep
         {
           "<leader>fg",
-          function()
-            fzf.live_grep_glob({ rg_opts = "--no-ignore-vcs " .. fzf.defaults.grep.rg_opts })
-          end,
+          fzf_cmd("live_grep_glob", { rg_opts = "--no-ignore-vcs " .. fzf.defaults.grep.rg_opts }),
           { desc = "Live Grep" },
         },
         {
           "<leader>fG",
-          fzf.live_grep_glob,
+          fzf_cmd("live_grep_glob"),
           { desc = "Live Grep" },
         },
         {
           "gS",
           require("util").grep_operator(function(query)
             local opts = { rg_opts = "--no-ignore-vcs " .. fzf.defaults.grep.rg_opts, search = query }
-            fzf.grep(opts)
+            fzf_cmd("grep", opts)()
           end),
           mode = { "n", "x" },
         },
         {
           "gs",
           require("util").grep_operator(function(query)
-            fzf.grep({ search = query })
+            fzf_cmd("grep", { search = query })()
           end),
           mode = { "n", "x" },
         },
 
         -- LSP
-        { "<leader>ss", fzf.lsp_document_symbols, { desc = "Goto Symbol" } },
-        { "<leader>sS", fzf.lsp_live_workspace_symbols, { desc = "Goto Symbol (Workspace)" } },
+        { "<leader>ss", fzf_cmd("lsp_document_symbols"), { desc = "Goto Symbol" } },
+        { "<leader>sS", fzf_cmd("lsp_live_workspace_symbols"), { desc = "Goto Symbol (Workspace)" } },
 
         -- Misc
-        { "<leader>sr", fzf.resume, { desc = "Resume" } },
+        { "<leader>sr", fzf_cmd("resume"), { desc = "Resume" } },
       }
     end,
     config = function()
