d10b3b978feae8e296c079f8ec3266c653bc2655
Timothy Mertz <mertzt89@gmail.com>
2024-07-31 09:48:56 -0500

    Add mini.ai plugin
    Add mini.ai plugin for enhanced treesitter object motions

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -115,6 +115,29 @@ function M.grep_operator(callback)
   end
 end
 
+function M.is_loaded(name)
+  local Config = require("lazy.core.config")
+  return Config.plugins[name] and Config.plugins[name]._.loaded
+end
+
+---@param name string
+---@param fn fun(name:string)
+function M.on_load(name, fn)
+  if M.is_loaded(name) then
+    fn(name)
+  else
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyLoad",
+      callback = function(event)
+        if event.data == name then
+          fn(name)
+          return true
+        end
+      end,
+    })
+  end
+end
+
 function M.opt_in(key, spec)
   local enabled_file = M.nvim_config_dir() .. "/.enable-" .. key
   local file = io.open(enabled_file, "rb")
