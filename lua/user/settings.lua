18673f5d6b884315a193c4ea76d7d036b5fa4906
Timothy Mertz <mertzt89@gmail.com>
2024-08-06 03:08:28 -0500

    Configure options for windows

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -67,3 +67,19 @@ end
 
 -- default folding to off
 vim.opt.foldenable = false
+
+-- Windows options
+if require("util").is_win() then
+  -- Use powershell for terminal
+  vim.opt.shell = "powershell.exe"
+  vim.opt.shellxquote = ""
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command "
+  vim.opt.shellquote = ""
+  vim.opt.shellpipe = "| Out-File -Encoding UTF8 %s"
+  vim.opt.shellredir = "| Out-File -Encoding UTF8 %s"
+end
+
+-- Neovide options
+if vim.g.neovide then
+  vim.opt.guifont = "Hack Nerd Font:h9"
+end
