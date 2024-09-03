fa731bde45b8ed61297f543a641ab160550441e0
Timothy Mertz <mertzt89@gmail.com>
2024-09-03 17:12:08 -0500

    clangd: Don't assume formatting if .clang-format is not present

--- lua/plugins/code/lang/c.lua
+++ lua/plugins/code/lang/c.lua
@@ -52,7 +52,7 @@ return {
             "--header-insertion=iwyu",
             "--completion-style=detailed",
             "--function-arg-placeholders",
-            "--fallback-style=llvm",
+            "--fallback-style=none",
           },
           init_options = {
             usePlaceholders = true,
