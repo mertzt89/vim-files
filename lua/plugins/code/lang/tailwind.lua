a32671f04d6c993b1bbe68f0014fd5462aa08b6a
Timothy Mertz <mertzt89@gmail.com>
2024-11-13 02:37:02 -0600

    tailwind: Add tsx files

--- lua/plugins/code/lang/tailwind.lua
+++ lua/plugins/code/lang/tailwind.lua
@@ -7,7 +7,7 @@ return {
           -- exclude a filetype from the default_config
           filetypes_exclude = { "markdown" },
           -- add additional filetypes to the default_config
-          filetypes_include = { "svelte" },
+          filetypes_include = { "svelte", "tsx" },
           -- to fully override the default_config, change the below
           -- filetypes = {}
         },
