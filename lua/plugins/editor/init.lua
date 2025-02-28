2493baf9efa588cd8676cdf2586ca0243f7b60e9
Timothy Mertz <mertzt89@gmail.com>
2025-02-28 12:46:37 -0600

    plugin: Add multicursor.nvim

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -4,5 +4,4 @@ return {
   { "tpope/vim-repeat" },
   { "numToStr/Comment.nvim", config = true, event = "LazyFile" },
   { "kyazdani42/nvim-web-devicons", lazy = true },
-  { "mg979/vim-visual-multi", event = "LazyFile" },
 }
