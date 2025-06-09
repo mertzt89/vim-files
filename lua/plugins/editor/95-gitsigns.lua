91031e95b638a5dc58bf77d57b1d0d4e29e269e8
Timothy Mertz <mertzt89@gmail.com>
2025-06-09 15:41:16 -0500

    plugin: Add gitsigns

---
+++ lua/plugins/editor/95-gitsigns.lua
@@ -0,0 +1,43 @@
+local keymap = function(key, cmd, desc)
+  return {
+    key,
+    function()
+      require("gitsigns")[cmd]()
+    end,
+    desc = desc,
+  }
+end
+
+return {
+  "lewis6991/gitsigns.nvim",
+  name = "gitsigns",
+  event = "VeryLazy",
+  -- See :help gitsigns-usage
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "➤" },
+      topdelete = { text = "➤" },
+      changedelete = { text = "▎" },
+    },
+  },
+
+  keys = {
+    -- Next Hunk
+    keymap("<leader>gj", "next_hunk", "Next Hunk"),
+    keymap("]g", "next_hunk", "Next Hunk"),
+
+    -- Previous Hunk
+    keymap("<leader>gk", "prev_hunk", "Previous Hunk"),
+    keymap("[g", "prev_hunk", "Previous Hunk"),
+
+    -- Staging/Resetting
+    keymap("<leader>gs", "stage_hunk", "Stage Hunk"),
+    keymap("<leader>gu", "undo_stage_hunk", "Undo Stage Hunk"),
+    keymap("<leader>gr", "reset_hunk", "Reset Hunk"),
+    keymap("<leader>gR", "reset_buffer", "Reset Buffer"),
+    keymap("<leader>gS", "stage_buffer", "Stage Buffer"),
+    keymap("<leader>gp", "preview_hunk", "Preview Hunk"),
+  },
+}
