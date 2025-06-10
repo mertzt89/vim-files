e481f64452f4cc96da7d7bdcd0ecb656f1e49a63
Timothy Mertz <mertzt89@gmail.com>
2025-06-10 15:25:01 -0500

    plugins: Add lazydev

--- lua/plugins/editor/95-blink-cmp.lua
+++ lua/plugins/editor/95-blink-cmp.lua
@@ -60,6 +60,11 @@ return {
     },
     -- allows extending the providers array elsewhere in your config
     -- without having to redefine it
-    opts_extend = { "sources.default" },
+    opts_extend = {
+      "sources.completion.enabled_providers",
+      "sources.compat",
+      "sources.default",
+      "sources.providers",
+    },
   },
 }
