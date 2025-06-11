7c95577274f54870f2bb0b7caacf7e34cf074679
Timothy Mertz <mertzt89@gmail.com>
2025-06-11 13:27:25 -0500

    plugin: Add mini.ai

---
+++ lua/plugins/editor/99-mini-ai.lua
@@ -0,0 +1,177 @@
+local M = {}
+
+-- From: https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/util/mini.lua#L10
+
+-- Mini.ai indent text object
+-- For "a", it will include the non-whitespace line surrounding the indent block.
+-- "a" is line-wise, "i" is character-wise.
+function M.ai_indent(ai_type)
+  local spaces = (" "):rep(vim.o.tabstop)
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local indents = {} ---@type {line: number, indent: number, text: string}[]
+
+  for l, line in ipairs(lines) do
+    if not line:find("^%s*$") then
+      indents[#indents + 1] = { line = l, indent = #line:gsub("\t", spaces):match("^%s*"), text = line }
+    end
+  end
+
+  local ret = {} ---@type {indent: number}[]
+  for i = 1, #indents do
+    if i == 1 or indents[i - 1].indent < indents[i].indent then
+      local from, to = i, i
+      for j = i + 1, #indents do
+        if indents[j].indent < indents[i].indent then
+          break
+        end
+        to = j
+      end
+      from = ai_type == "a" and from > 1 and from - 1 or from
+      to = ai_type == "a" and to < #indents and to + 1 or to
+      ret[#ret + 1] = {
+        indent = indents[i].indent,
+        from = { line = indents[from].line, col = ai_type == "a" and 1 or indents[from].indent + 1 },
+        to = { line = indents[to].line, col = #indents[to].text },
+      }
+    end
+  end
+
+  return ret
+end
+
+-- taken from MiniExtra.gen_ai_spec.buffer
+function M.ai_buffer(ai_type)
+  local start_line, end_line = 1, vim.fn.line("$")
+  if ai_type == "i" then
+    -- Skip first and last blank lines for `i` textobject
+    local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+    -- Do nothing for buffer with all blanks
+    if first_nonblank == 0 or last_nonblank == 0 then
+      return { from = { line = start_line, col = 1 } }
+    end
+    start_line, end_line = first_nonblank, last_nonblank
+  end
+
+  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+  return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+end
+
+-- register all text objects with which-key
+---@param opts table
+function M.map_keys(opts)
+  local wk = require("which-key")
+
+  local objects = {
+    { " ", desc = "whitespace" },
+    { '"', desc = '" string' },
+    { "'", desc = "' string" },
+    { "(", desc = "() block" },
+    { ")", desc = "() block with ws" },
+    { "<", desc = "<> block" },
+    { ">", desc = "<> block with ws" },
+    { "?", desc = "user prompt" },
+    { "U", desc = "use/call without dot" },
+    { "[", desc = "[] block" },
+    { "]", desc = "[] block with ws" },
+    { "_", desc = "underscore" },
+    { "`", desc = "` string" },
+    { "a", desc = "argument" },
+    { "b", desc = ")]} block" },
+    { "c", desc = "class" },
+    { "d", desc = "digit(s)" },
+    { "e", desc = "CamelCase / snake_case" },
+    { "f", desc = "function" },
+    { "g", desc = "entire file" },
+    { "i", desc = "indent" },
+    { "o", desc = "block, conditional, loop" },
+    { "q", desc = "quote `\"'" },
+    { "t", desc = "tag" },
+    { "u", desc = "use/call" },
+    { "{", desc = "{} block" },
+    { "}", desc = "{} with ws" },
+  }
+
+  ---@type table<string, string>
+  local mappings = vim.tbl_extend("force", {}, {
+    around = "a",
+    inside = "i",
+    around_next = "an",
+    inside_next = "in",
+    around_last = "al",
+    inside_last = "il",
+    goto_left = "g[",
+    goto_right = "g]",
+  }, opts.mappings or {})
+
+  -- Generate mappings for "o" and "x" modes
+  local ret = { mode = { "o", "x" } }
+  for name, prefix in pairs(mappings) do
+    if name:gmatch("around_")() ~= nil or name:gmatch("inside_")() ~= nil then
+      name = name:gsub("around_", ""):gsub("inside_", "")
+      ret[#ret + 1] = { prefix, group = name }
+      for _, obj in ipairs(objects) do
+        local desc = obj.desc
+        if prefix:sub(1, 1) == "i" then
+          desc = desc:gsub(" with ws", "")
+        end
+        --- @class wk.Spec
+        local mapping = { prefix .. obj[1], desc = obj.desc }
+        ret[#ret + 1] = mapping
+      end
+    end
+  end
+  wk.add(ret, { notify = false })
+
+  -- Generate mappings for "n" mode
+  ret = { mode = "n" }
+  for name, prefix in pairs(mappings) do
+    if name:gmatch("goto_")() ~= nil then
+      name = name:gsub("goto_", "")
+      ret[#ret + 1] = { prefix, group = name }
+      for _, obj in ipairs(objects) do
+        local desc = obj.desc
+        desc = desc:gsub(" with ws", "")
+
+        --- @class wk.Spec
+        local mapping = { prefix .. obj[1], mode = "n", desc = desc }
+        ret[#ret + 1] = mapping
+      end
+    end
+  end
+  wk.add(ret, { notify = false })
+end
+
+return {
+  "echasnovski/mini.ai",
+  event = "VeryLazy",
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({ -- code block
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }),
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+        d = { "%f[%d]%d+" }, -- digits
+        e = { -- Word with case
+          { "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+          "^().*()$",
+        },
+        i = M.ai_indent, -- indent
+        g = M.ai_buffer, -- buffer
+        u = ai.gen_spec.function_call(), -- u for "Usage"
+        U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+      },
+    }
+  end,
+  config = function(_, opts)
+    require("mini.ai").setup(opts)
+    require("util.module").on_load("which-key.nvim", function()
+      M.map_keys(opts)
+    end)
+  end,
+}
--- lua/util/module.lua
+++ lua/util/module.lua
@@ -31,6 +31,33 @@ function M.foreach_lua_do(path, func)
   end
 end
 
+--- Check if a plugin is loaded
+---@param name string
+function M.is_loaded(name)
+  local Config = require("lazy.core.config")
+  return Config.plugins[name] and Config.plugins[name]._.loaded
+end
+
+--- Register a callback to be called when a plugin is loaded, if the 
+--- plugin is already loaded, it will call the function immediately.
+---@param name string
+---@param fn fun(name:string)
+function M.on_load(name, fn)
+  if M.is_loaded(name) then
+    fn(name)
+  else
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyLoad",
+      callback = function(event)
+        if event.data == name then
+          fn(name)
+          return true
+        end
+      end,
+    })
+  end
+end
+
 function M.parent(module)
   local parts = vim.split(module, ".", { plain = true })
   if #parts == 1 then
