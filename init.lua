28fd8ca3799d948e67066f7f5096754668c8f45b
Timothy Mertz <mertzt89@gmail.com>
2025-06-17 15:03:15 -0500

    Add custom lazy.nvim keybinds

--- init.lua
+++ init.lua
@@ -12,4 +12,49 @@ require("lazy").setup({
   },
   -- automatically check for plugin updates
   checker = { enabled = true },
+
+  ui = {
+    custom_keys = {
+      -- Open lazygit log for a plugin
+      ["gL"] = {
+        function(plugin)
+          Snacks.terminal.open({ "lazygit", "log" }, {
+            cwd = plugin.dir,
+          })
+        end,
+        desc = "Open lazygit log",
+      },
+
+      -- Inspect a plugin
+      ["gi"] = {
+        function(plugin)
+          Snacks.notify(vim.inspect(plugin), {
+            title = "Inspect " .. plugin.name,
+            ft = "lua",
+          })
+        end,
+        desc = "Inspect Plugin",
+      },
+
+      -- Open plugin directory in terminal
+      ["gt"] = {
+        function(plugin)
+          local lazy_win = require("lazy.view").view.win
+
+          Snacks.terminal.open(nil, {
+            cwd = plugin.dir,
+            start_insert = false,
+          })
+
+          if lazy_win and vim.api.nvim_win_is_valid(lazy_win) then
+            vim.notify("Settting window to " .. lazy_win, vim.log.levels.INFO, {
+              title = "Inspect Plugin",
+            })
+            vim.api.nvim_set_current_win(lazy_win)
+          end
+        end,
+        desc = "Open terminal in plugin dir",
+      },
+    },
+  },
 })
