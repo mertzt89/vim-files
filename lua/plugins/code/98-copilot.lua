975a2b2b64a6667f631c6f15e5efc73e1b88eab0
Timothy Mertz <mertzt89@gmail.com>
2025-10-22 11:37:01 -0500

    Replace CopilotChat with sidekick

--- lua/plugins/code/98-copilot.lua
+++ lua/plugins/code/98-copilot.lua
@@ -1,7 +1,8 @@
 local copilotState = {}
 
-local native_copilot = vim.lsp.inline_completion ~= nil
-if native_copilot then
+local enableCopilotLSP = vim.lsp.inline_completion ~= nil
+
+if enableCopilotLSP then
   ---@alias copilot_status_notification_data { status: ''|'Normal'|'InProgress'|'Warning', message: string }
 
   ---@param result copilot_status_notification_data
@@ -51,100 +52,16 @@ else
   vim.notify("LSP Inline Completion not available (available on nightly)", vim.log.levels.WARN)
 end
 
----@param kind string
-local function pick(kind)
-  return function()
-    local actions = require("CopilotChat.actions")
-    local items = actions[kind .. "_actions"]()
-    if not items then
-      vim.notify("No " .. kind .. " found on the current line", vim.log.levels.WARN)
-      return
-    end
-    require("CopilotChat.integrations.fzflua").pick(items)
-  end
-end
-
 return {
   -- Mason: Install copilot-language-server
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = { "copilot-language-server" },
-    },
-  },
-
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
-    cmd = "CopilotChat",
-    opts = function()
-      local user = vim.env.USER or "User"
-      user = user:sub(1, 1):upper() .. user:sub(2)
-      return {
-        model = "gpt-5",
-        auto_insert_mode = true,
-        show_help = true,
-        question_header = "  " .. user .. " ",
-        answer_header = "  Copilot ",
-        window = {
-          width = 0.4,
+  enableCopilotLSP
+      and {
+        "williamboman/mason.nvim",
+        opts = {
+          ensure_installed = { "copilot-language-server" },
         },
-        selection = function(source)
-          local select = require("CopilotChat.select")
-          return select.visual(source) or select.buffer(source)
-        end,
       }
-    end,
-    keys = {
-      { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
-      { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
-      {
-        "<leader>aa",
-        function()
-          return require("CopilotChat").toggle()
-        end,
-        desc = "Toggle (CopilotChat)",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>ax",
-        function()
-          return require("CopilotChat").reset()
-        end,
-        desc = "Clear (CopilotChat)",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>aq",
-        function()
-          local input = vim.fn.input("Quick Chat: ")
-          if input ~= "" then
-            require("CopilotChat").ask(input)
-          end
-        end,
-        desc = "Quick Chat (CopilotChat)",
-        mode = { "n", "v" },
-      },
-      -- Show help actions with telescope
-      { "<leader>ad", pick("help"), desc = "Diagnostic Help (CopilotChat)", mode = { "n", "v" } },
-      -- Show prompts actions with telescope
-      { "<leader>ap", pick("prompt"), desc = "Prompt Actions (CopilotChat)", mode = { "n", "v" } },
-    },
-    config = function(_, opts)
-      local chat = require("CopilotChat")
-      -- require("CopilotChat.integrations.cmp").setup()
-
-      vim.api.nvim_create_autocmd("BufEnter", {
-        pattern = "copilot-chat",
-        callback = function()
-          vim.opt_local.relativenumber = false
-          vim.opt_local.number = false
-        end,
-      })
-
-      chat.setup(opts)
-    end,
-  },
+    or nil,
 
   -- Blink CMP source for Copilot
   {
