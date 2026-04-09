76928f666b0881265fffaa7d51b17dc47f2c1f6c
Timothy Mertz <mertzt89@gmail.com>
2026-04-09 14:29:09 -0500

    Fix svelte

---
+++ after/queries/svelte/injections.scm
@@ -0,0 +1,5 @@
+; inherits: svelte
+((script_element
+   (start_tag (tag_name) @_tag)
+   (raw_text) @typescript)
+ (#eq? @_tag "script"))
--- lua/plugins/code/00-treesitter.lua
+++ lua/plugins/code/00-treesitter.lua
@@ -2,6 +2,31 @@
 -- Plugin: Treesitter
 ------------------------------------------------------------
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "*" },
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    if not vim.treesitter.language.add(lang) then
+      local available = vim.g.ts_available
+          or require("nvim-treesitter").get_available()
+      if not vim.g.ts_available then
+        vim.g.ts_available = available
+      end
+      if vim.tbl_contains(available, lang) then
+        require("nvim-treesitter").install(lang)
+      end
+    end
+
+    if vim.treesitter.language.add(lang) then
+      vim.treesitter.start(args.buf, lang)
+      vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.wo[0][0].foldmethod = "expr"
+    end
+  end,
+})
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
