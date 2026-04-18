9da4a0c7559544e65d40ae1a75f26c6ce1e09fef
Timothy Mertz <mertzt89@gmail.com>
2026-04-18 00:02:06 -0500

    Add autocommand to refresh buffer contents on focus

--- lua/settings/autocommands.lua
+++ lua/settings/autocommands.lua
@@ -8,3 +8,19 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>quit<cr>", { silent = true, buffer = true })
   end,
 })
+
+-- Check if files have changed when the focus is regained
+vim.api.nvim_create_autocmd("FocusGained", {
+  desc = "Reload files from disk when we focus vim",
+  pattern = "*",
+  command = "if getcmdwintype() == '' | checktime | endif",
+  group = group,
+})
+
+-- Check if files have changed when entering the buffer
+vim.api.nvim_create_autocmd("BufEnter", {
+  desc = "Every time we enter an unmodified buffer, check if it changed on disk",
+  pattern = "*",
+  command = "if &buftype == '' && !&modified && expand('%') != '' | exec 'checktime ' . expand('<abuf>') | endif",
+  group = group,
+})
